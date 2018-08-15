b2a3eb56ba24b84f437a9308a17aa2a6cdcece30
josh <joshfeli613@gmail.com>
2018-08-15 15:27:28 -0400

    made multiple pages

---
+++ COPY_MASTER.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const fileNames = [
+    "Theory",
+    "Composition",
+    "Woodwinds",
+    "Brass",
+    "Percussion",
+    "Strings",
+    "Keyboards",
+    "Technique",
+    "How to Read",
+    "Sight Reading",
+    "Music History",
+    "Miscellaneous",
+];
+
+for(const name of fileNames){
+    fs.createReadStream('MASTER.html')
+        .pipe(fs.createWriteStream("forums/"+name+".html"));
+}
